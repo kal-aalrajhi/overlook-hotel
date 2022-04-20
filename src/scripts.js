c1a950e6171fe058988c78ca791cf13370d585e6
Kal <kal.aalrajhi@gmail.com>
2022-04-20 10:20:55 -0600

    Add subheading

--- src/scripts.js
+++ src/scripts.js
@@ -4,8 +4,8 @@
 // An example of how you tell webpack to use a CSS (SCSS) file
 import './css/styles.css';
 import './css/utilities.css';
-import './src/images/hotel-main.jpg';
-import './src/images/hotel-room-icon.png';
+// import './images/hotel-main.jpg';
+// import './images/hotel-room-icon.png';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 // import './images/turing-logo.png'
