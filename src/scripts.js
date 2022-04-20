f8abb29470ae8656e7aeb89a59c15a9caff99b5a
Kal <kal.aalrajhi@gmail.com>
2022-04-20 11:18:03 -0600

    Add booking cards

--- src/scripts.js
+++ src/scripts.js
@@ -7,6 +7,8 @@ import './css/utilities.css';
 import './images/hotel-main.jpg';
 import './images/hotel-room-icon.png';
 import './images/calendar-icon.png';
+import './images/one-bed-icon.png';
+import './images/two-bed-icon.png';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 // import './images/turing-logo.png'
