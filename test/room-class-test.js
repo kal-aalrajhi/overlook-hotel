d9f2f2625e7f5fa2c816e7c797478a5608c1e6d6
Kal <kal.aalrajhi@gmail.com>
2022-04-21 13:30:32 -0600

    Add test template and class tempalte

---
+++ test/room-class-test.js
@@ -0,0 +1,11 @@
+import { expect } from 'chai';
+import { Room } from '../src/classes/Room';
+import { roomsSampleData } from '../src/data/rooms-sample-data';
+
+describe('Room', () => {
+
+  beforeEach(() => {
+
+  });
+
+});
